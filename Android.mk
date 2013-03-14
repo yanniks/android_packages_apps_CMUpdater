f37c47df1733101d106efed1308ccac943b6c69e
yanniks <kontakt@yanniks.de>
2013-03-14 15:16:04 +0100

    updated Android.mk

--- Android.mk
+++ Android.mk
@@ -1,35 +1,8 @@
-LOCAL_PATH:= $(call my-dir)
-
-ifneq ($(TARGET_RECOVERY_FSTAB),)
-  recovery_fstab := $(strip $(wildcard $(TARGET_RECOVERY_FSTAB)))
-else
-  recovery_fstab := $(strip $(wildcard $(TARGET_DEVICE_DIR)/recovery.fstab))
-endif
-
-ALTERNATE_IS_INTERNAL := false
-ifneq ($(recovery_fstab),)
-  recovery_fstab := $(ANDROID_BUILD_TOP)/$(recovery_fstab)
-  ifneq ($(shell grep "/emmc" $(recovery_fstab)),)
-  ALTERNATE_IS_INTERNAL := true
-  endif
-endif
+LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-
-LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
-
-ifeq ($(ALTERNATE_IS_INTERNAL), true)
-  LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res-compat $(LOCAL_RESOURCE_DIR)
-endif
-
-LOCAL_MODULE_TAGS := optional
-
-LOCAL_SRC_FILES := $(call all-java-files-under, src)
-LOCAL_SRC_FILES += src/com/cyanogenmod/updater/interfaces/IUpdateCheckServiceCallback.aidl \
-		src/com/cyanogenmod/updater/interfaces/IUpdateCheckService.aidl
-
+LOCAL_SRC_FILES := $(call all-subdir-java-files)
 LOCAL_PACKAGE_NAME := CMUpdater
-
-LOCAL_PROGUARD_FLAGS := -include $(LOCAL_PATH)/proguard.flags
-
+LOCAL_CERTIFICATE := platform
 include $(BUILD_PACKAGE)
+
